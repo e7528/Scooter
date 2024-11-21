e60c4cac79405512a61c61a2049985c922977702
e7528 <135951099+e7528@users.noreply.github.com>
2024-11-21 11:06:28 +0800

    Update scooterDemo.cpp

--- scooterDemo.cpp
+++ scooterDemo.cpp
@@ -5,36 +5,41 @@ using namespace std;
 
 string pickScooter(int choice) {
 
-	string scooter1 = "Kiddie Scooter";
-	string scooter2 = "3 Wheel Scooter";
-	string scooter3 = "2 Wheel Scooter";
+    string scooter1 = "Kiddie Scooter";
+    string scooter2 = "3 Wheel Scooter";
+    string scooter3 = "2 Wheel Scooter";
 
 
-	switch (choice) {
-	    case 1:
-		    cout << "You chose the Kiddie Scooter!\n";
-			return scooter1;
-		    break;
-		case 2:
-			cout << "You chose the 3 Wheel Scooter!\n";
-			return scooter2;
-			break;
-		case 3:
-			cout << "You chose the 2 Wheel Scooter!\n";
-			return scooter3;
-			break;
-	}
+    switch (choice) {
+    case 1:
+        cout << "You chose the Kiddie Scooter!\n";
+        return scooter1;
+        break;
+    case 2:
+        cout << "You chose the 3 Wheel Scooter!\n";
+        return scooter2;
+        break;
+    case 3:
+        cout << "You chose the 2 Wheel Scooter!\n";
+        return scooter3;
+        break;
+    default:
+        return 0;
+    }
 
 }
 
 int main() {
 
-	int scooterPicked;
+    int scooterPicked;
 
-	cin >> scooterPicked;
+    cout << "Please enter your choice" << endl;
+    cout << "[1] Kiddie Scooter" << endl;
+    cout << "[2] 3 Wheel Scooter" << endl;
+    cout << "[3] 2 Wheel Scooter" << endl;
+    cin >> scooterPicked;
+    pickScooter(scooterPicked);
 
-	pickScooter(scooterPicked);
-	
 
-	return 0;
+    return 0;
 }
